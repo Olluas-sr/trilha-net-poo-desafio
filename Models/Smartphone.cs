e6378d2ef8e9f9d869beafdb889dc68e20f8e68b
Saullo Santos <saullo.s.r@hotmail.com>
2024-05-06 20:59:59 -0300

    Codigo do desafio aplicado.

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,10 +4,16 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }
+        private string Imei { get; set; }
+        private int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
             // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
@@ -20,7 +26,7 @@ namespace DesafioPOO.Models
         {
             Console.WriteLine("Recebendo ligação...");
         }
-
+        
         public abstract void InstalarAplicativo(string nomeApp);
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
-
+Smartphone nokia = new Nokia(numero: "84427788", modelo:"Flip", imei: "556688", memoria:128);
+Console.WriteLine($"Nokia sendo iniciado... ");
+nokia.Ligar();
+nokia.InstalarAplicativo("ZipZop");
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("\n");
+
+Smartphone iphone = new Iphone(numero: "9955588", modelo:"Ix84", imei: "998822", memoria:240);
+Console.WriteLine($"Iphone sendo iniciado... ");
+iphone.Ligar();
+nokia.InstalarAplicativo("Telegran");
